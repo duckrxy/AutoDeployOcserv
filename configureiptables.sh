bd32210dc718859d7f1c962359d71fec366b40d0
Ubuntu <azureuser@vpndev.vpndev.d6.internal.cloudapp.net>
2015-06-09 07:17:03 +0000

    flush existing forward rules before config iptables

--- configureiptables.sh
+++ configureiptables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo iptables -F FORWARD
 sudo iptables -t nat -A POSTROUTING -s 10.10.0.0/24 -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -s 10.10.0.0/24 -j ACCEPT
 sudo mkdir /etc/iptables
