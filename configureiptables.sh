71349ffa3ca68928398a4c0ba007e04e1addc0bb
Ubuntu <azureuser@vpndev.vpndev.d6.internal.cloudapp.net>
2015-06-09 07:11:22 +0000

    add iptables config

--- configureiptables.sh
+++ configureiptables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-iptables -t nat -A POSTROUTING -s 10.10.0.0/24 -o eth0 -j MASQUERADE
-iptables -A FORWARD -s 10.10.0.0/24 -j ACCEPT
-mkdir /etc/iptables
-touch /etc/iptables/rules.v4
-chmod 777 /etc/iptables/rules.v4
+sudo iptables -t nat -A POSTROUTING -s 10.10.0.0/24 -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -s 10.10.0.0/24 -j ACCEPT
+sudo mkdir /etc/iptables
+sudo touch /etc/iptables/rules.v4
+sudo chmod 777 /etc/iptables/rules.v4
 
-iptables-save > /etc/iptables/rules.v4
+sudo iptables-save > /etc/iptables/rules.v4
 
-sed -i "s/^#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/"
+sudo sed -i "s/^#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 
-sysctl -p /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
 
