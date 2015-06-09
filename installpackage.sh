71dc344bfc79ec63bffb11f9cd5831fe32460497
Ubuntu <azureuser@vpndev.vpndev.d6.internal.cloudapp.net>
2015-06-09 06:29:42 +0000

    commit change

--- installpackage.sh
+++ installpackage.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo apt-get update
+
+sudo apt-get install build-essential pkg-config libgnutls28-dev libreadline-dev libseccomp-dev libpam0g-dev libwrap0-dev libnl-nf-3-dev
+
+sudo apt-get install gnutls-bin
--- script.sh
+++ script.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
-echo hello
+echo Starting deploy ocserv 
 # Go to current user's home directory
 
 cd ~
 
-wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.10.4.tar.xz
 
+echo Install dependency package
+
+wget https://raw.githubusercontent.com/duckrxy/AutoDeployOcserv/master/installpackage.sh
+
+sudo chmod +x ./installpackage.sh
+sudo ./installpackage.sh
+
+#sudo apt-get update
+
+#sudo apt-get install build-essential pkg-config libgnutls28-dev libreadline-dev libseccomp-dev libpam0g-dev libwrap0-dev libnl-nf-3-dev
+
+#sudo apt-get install gnutls-bin
+echo Install Ocserv
+
+wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.10.4.tar.xz
 
 tar xvf ocserv-0.10.4.tar.xz 
 
-sudo apt-get update
-
-sudo apt-get install build-essential pkg-config libgnutls28-dev libreadline-dev libseccomp-dev libpam0g-dev libwrap0-dev libnl-nf-3-dev
-
 cd ocserv-0.10.4
 
-#`./configure
+#./configure
 
 #make
 
 #sudo make install
 
+wget https://raw.githubusercontent.com/duckrxy/AutoDeployOcserv/master/ocserv.conf
 sudo mkdir /etc/ocserv
 
-sudo cp doc/sample.config /etc/ocserv/ocserv.conf
+sudo cp ./ocserv.config /etc/ocserv/ocserv.conf
+
+echo Generate Certificates
+cd ~
+wget https://raw.githubusercontent.com/duckrxy/AutoDeployOcserv/master/generatecert.sh
+
+sudo chmod +x ./generatecert.sh
+
+./generatecert.sh
